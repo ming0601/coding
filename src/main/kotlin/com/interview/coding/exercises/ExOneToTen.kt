65eb04d7cc97f490d60e50e5144d7de32e3aff80
ming0601 <fangming0601@gmail.com>
2019-04-27 23:45:36 +0200

    Ex2 Algo allow duplicates at most 2 + tests

--- src/main/kotlin/com/interview/coding/exercises/ExOneToTen.kt
+++ src/main/kotlin/com/interview/coding/exercises/ExOneToTen.kt
@@ -3,13 +3,56 @@ package com.interview.coding.exercises
 class ExOneToTen {
 
     /**
-     * Given a sorted array, remove the duplicates in place such that each element appear only once and
+     * Ex1: Given a sorted array, remove the duplicates in place such that each element appear only once and
      * return the new length.
      */
     fun removeDuplicatesFromSortedArray(input: Array<Int?>?): List<Int> {
         return when {
             input.isNullOrEmpty() -> listOf()
             else -> input.filterNotNull().distinct()
+        }
+    }
+
+    /**
+     * Ex2: Follow up for "Remove Duplicates": What if duplicates are allowed at most twice
+     */
+    fun allowDuplicatesAtMostTwice(input: Array<Int?>?): List<Int> {
+        return when {
+            input.isNullOrEmpty() -> emptyList()
+            input.size < 3 -> input.filterNotNull().sorted()
+            else -> {
+                val filteredList = input.filterNotNull().sorted().toMutableList()
+                val result = mutableListOf<Int>()
+                var i = 0
+                var j = 1
+                while (j < filteredList.size) {
+                    if (filteredList[j] == filteredList[i]) {
+                        when {
+                            i == 0 -> {
+                                result.add(filteredList[i])
+                                result.add(filteredList[j])
+                                i++
+                                j++
+                            }
+
+                            filteredList[i] == filteredList[i-1] -> j++
+
+                            else -> {
+                                i++
+                                filteredList[i] = filteredList[j]
+                                result.add(filteredList[i])
+                                j++
+                            }
+                        }
+                    } else {
+                        i++
+                        filteredList[i] = filteredList[j]
+                        result.add(filteredList[i])
+                        j++
+                    }
+                }
+                return result
+            }
         }
     }
 }
--- src/test/kotlin/com/interview/coding/exercises/ExOneToTenTest.kt
+++ src/test/kotlin/com/interview/coding/exercises/ExOneToTenTest.kt
@@ -38,4 +38,33 @@ class ExOneToTenTest {
         Assertions.assertEquals(expectedResult, result)
         Assertions.assertEquals(expectedSize, result.size)
     }
+
+    @DisplayName("Testing Ex2: Duplicates are allowed at most twice")
+    @ParameterizedTest(name = "Case {index} : Remove duplicates according to {0} value(s) in the array")
+    @CsvSource("null,0", "2,2", "arrayOfNulls,0", "2_With_1_Null,1", "moreThan_2,8", "moreThan_2_WithNulls,10")
+    fun testEx2(case: String, expectedSize: Int) {
+        val input: Array<Int?>? = when (case) {
+            "null" -> null
+            "2" -> arrayOf(1,2)
+            "arrayOfNulls" -> arrayOf(null,null)
+            "2_With_1_Null" -> arrayOf(1,null)
+            "moreThan_2" -> arrayOf(1,2,3,4,5,1,2,3)
+            "moreThan_2_WithNulls" -> arrayOf(1,null,3,4,null,1,2,3,null,2,3,4,2,3,1,5,5)
+            else -> fail("Should not come here!")
+        }
+
+        val expectedResult = when (case) {
+            "null" -> listOf()
+            "2" -> listOf(1,2)
+            "arrayOfNulls" -> listOf()
+            "2_With_1_Null" -> listOf(1)
+            "moreThan_2" -> listOf(1,1,2,2,3,3,4,5)
+            "moreThan_2_WithNulls" -> listOf(1,1,2,2,3,3,4,4,5,5)
+            else -> fail("Should not come here!")
+        }
+
+        val result = subject.allowDuplicatesAtMostTwice(input)
+        Assertions.assertEquals(expectedResult, result)
+        Assertions.assertEquals(expectedSize, result.size)
+    }
 }
